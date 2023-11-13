ada00f2a9ce4e595c975f1e3f1c86287e43496fb
Joonatan Kytönen <joonatan.kytonen@gmail.com>
2023-11-13 14:07:29 +0200

    Game map, snake movement, collision with map borders shuts down game

--- main.py
+++ main.py
@@ -3,18 +3,27 @@ import pygame
 
 # Initializing Pygame
 pygame.init()
-screen = pygame.display.set_mode((1000, 1000))
+Screen_Height=1000
+Screen_Width=1000
+screen = pygame.display.set_mode((Screen_Height, Screen_Width))
 clock = pygame.time.Clock()
 running=True
 dt=0
+points=[(250,250),(250, 750), (750,750), (750, 250)]
 
 # Colors
 border_color = (255, 0, 0)
 snake_color = (0, 255, 0)
 
-# Snake attributes
-snake_pos=pygame.Vector2(screen.get_width()/2, screen.get_height()/2)
-snake_speed=100
+# Snake
+snake=pygame.Rect((Screen_Height/2),(Screen_Width/2),25,25)
+
+# Map
+left=pygame.Rect(250, 250, 2, 500)
+top=pygame.Rect(250,250,500,2)
+right=pygame.Rect(750, 250, 2, 500)
+bottom=pygame.Rect(250, 750, 500, 2)
+
 
 # Game starts
 while running:
@@ -23,28 +32,41 @@ while running:
         if event.type == pygame.QUIT:
             running=False
 
+
     # Clear the screen of anything from last frame
     screen.fill((0,0,0))
-    # Draw the game area
-    pygame.draw.rect(screen, border_color, pygame.Rect(250, 250, 500, 500), 2)
+    # Draw the map
+    pygame.draw.rect(screen, border_color, left)
+    pygame.draw.rect(screen, border_color, top)
+    pygame.draw.rect(screen, border_color, right)
+    pygame.draw.rect(screen, border_color, bottom)
+    #asd = pygame.draw.lines(screen, border_color, False, points, 2)
     # Draw snake
-    pygame.draw.circle(screen, snake_color, snake_pos, 10)
+    pygame.draw.rect(screen, snake_color, snake)
 
-    # Move
+    # Move snake
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        snake_pos.y -= snake_speed*dt
-    if keys[pygame.K_a]:
-        snake_pos.x -= snake_speed*dt
-    if keys[pygame.K_s]:
-        snake_pos.y += snake_speed*dt
-    if keys[pygame.K_d]:
-        snake_pos.x += snake_speed*dt
+        snake.move_ip(0,-1)
+    elif keys[pygame.K_a]:
+        snake.move_ip(-1,0)
+    elif keys[pygame.K_s]:
+        snake.move_ip(0,1)
+    elif keys[pygame.K_d]:
+        snake.move_ip(1,0)
 
-    if snake_pos.y == 252:
-        running=False
+    if snake.colliderect(left):
+        running = False
+    elif snake.colliderect(top):
+        running = False
+    elif snake.colliderect(right):
+        running = False
+    elif snake.colliderect(bottom):
+        running = False
+
     pygame.display.flip()
 
+    # Run game on 60fps
     dt=clock.tick(60)/1000
 
 pygame.quit()
